aeb388d9d071d475935ab43ad3e1b09d976442b6
69DABLUI69 <bruhmemebrodude69@gmail.com>
2026-01-28 15:31:31 +0200

    gifs and links

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -3,21 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.dachat.app',
   appName: 'DaChat',
-  webDir: 'out',
-  server: {
-    url: 'https://dachat-app.vercel.app', 
-    cleartext: true
-  },
-  plugins: {
-    StatusBar: {
-      style: 'Dark',
-      backgroundColor: '#2d0055', // REPLACE this with your exact purple hex color
-    },
-    NavigationBar: {
-      backgroundColor: '#2d0055', // REPLACE this with your exact purple hex color
-      style: 'Dark'
-    }
-  }
+  webDir: 'public'
 };
 
 export default config;
